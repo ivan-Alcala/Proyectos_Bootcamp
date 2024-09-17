58bdc86985fcf70467c8333cd6d60aff3bb1b7a2
Ivan Alcala Muñoz <alcalamunozivan@gmail.com>
2024-09-17 12:49:05 +0200

    Creada clase Card con los valores

---
+++ POO/Class/Card.cs
@@ -0,0 +1,19 @@
+﻿namespace POO.Class
+{
+    public class Card
+    {
+        public string Suit { get; private set; }
+        public int Value { get; private set; }
+
+        public Card(string suit, int value)
+        {
+            Suit = suit;
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{Value} de {Suit}";
+        }
+    }
+}
