d0fca9f1f54f6b1ba197c71eae0a0b592a3e4dff
Ivan Alcala Muñoz <alcalamunozivan@gmail.com>
2024-09-17 13:05:22 +0200

    Nueva clase con valores Player

---
+++ POO/Class/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace POO.Class
+{
+    public class Player
+    {
+        public string Name { get; private set; }
+        public Queue<Card> Cards { get; private set; }
+
+        public Player(string name)
+        {
+            Name = name;
+            Cards = new Queue<Card>();
+        }
+    }
+}
