9ff6b5bf49113f014e6de562e38f180cef52ee92
Ivan Alcala Muñoz <alcalamunozivan@gmail.com>
2024-10-28 13:43:49 +0100

    Creado UserControl para el MedicalRecord y el Appointment vacios

--- WinFormGestionHospital/Forms/UserControlPersons.Designer.cs
+++ WinFormGestionHospital/Forms/UserControlPersons.Designer.cs
@@ -96,6 +96,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.Transparent;
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.fwLtPnHospitalOptions);
             this.Name = "UserControlPersons";
