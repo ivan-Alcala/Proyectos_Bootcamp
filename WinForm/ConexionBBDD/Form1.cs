d5e538f5330411443f4835e50417b6e8eb4e0903
Ivan Alcala Muñoz <alcalamunozivan@gmail.com>
2024-11-05 10:15:00 +0100

    Eventos para conectar/desconectar botones de la conexion a la BBDD

--- WinForm/ConexionBBDD/Form1.cs
+++ WinForm/ConexionBBDD/Form1.cs
@@ -1,12 +1,42 @@
-﻿using System.Windows.Forms;
+﻿using ConexionBBDD.Class;
+using System;
+using System.Windows.Forms;
 
 namespace ConexionBBDD
 {
     public partial class Form1 : Form
     {
+        private BBDDConnect dbConnection;
+
         public Form1()
         {
             InitializeComponent();
+            dbConnection = new BBDDConnect();
+            lbBBDDConnectionStates.Text = "Desconectado";
+            btBBDDDisconect.Enabled = false;
+        }
+
+        private void btBBDDConect_Click(object sender, EventArgs e)
+        {
+            if (dbConnection.Connect())
+            {
+                lbBBDDConnectionStates.Text = "Conectado";
+                btBBDDConect.Enabled = false;
+                btBBDDDisconect.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("Error al conectar a la base de datos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                lbBBDDConnectionStates.Text = "Error en la conexión";
+            }
+        }
+
+        private void btBBDDDisconect_Click(object sender, EventArgs e)
+        {
+            dbConnection.Disconnect();
+            lbBBDDConnectionStates.Text = "Desconectado";
+            btBBDDConect.Enabled = true;
+            btBBDDDisconect.Enabled = false;
         }
     }
 }
