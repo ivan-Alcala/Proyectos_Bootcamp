46d8a5a32716f7377f038b30b1d30ef1ca2b6563
Ivan Alcala Muñoz <alcalamunozivan@gmail.com>
2024-11-05 10:33:35 +0100

    Clase Job vacia creada, de la BBDD de employee

---
+++ WinForm/ConexionBBDD/Class/Model/Job.cs
@@ -0,0 +1,10 @@
+﻿namespace ConexionBBDD.Class.Model
+{
+    public class Job
+    {
+        public int JobId { get; set; } // job_id
+        public string JobTitle { get; set; } // job_title
+        public float MinSalary { get; set; } // min_salary
+        public float MaxSalary { get; set; } // max_salary
+    }
+}
