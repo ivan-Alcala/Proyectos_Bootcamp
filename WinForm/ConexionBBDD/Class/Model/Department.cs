bb8282c5077fa63f32ea50d0d8a44ae544dfb91b
Ivan Alcala Muñoz <alcalamunozivan@gmail.com>
2024-11-08 09:53:52 +0100

    Creada todas las clases del modelo con sus datos (Region, Location, Department, Employee, Dependent, Country)

---
+++ WinForm/ConexionBBDD/Class/Model/Department.cs
@@ -0,0 +1,9 @@
+﻿namespace ConexionBBDD.Class.Model
+{
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+        public int? LocationId { get; set; }
+    }
+}
---
+++ WinForm/ConexionBBDD/Class/Model/Region.cs
@@ -0,0 +1,8 @@
+﻿namespace ConexionBBDD.Class.Model
+{
+    public class Region
+    {
+        public int RegionId { get; set; }
+        public string RegionName { get; set; }
+    }
+}
